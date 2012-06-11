0125a77b3c0b5c3fbc91f66b2e07ff972a54fea8
Fred <Fred@192.168.1.107>
2012-06-10 20:39:35 -0500

    ImageLoader

---
+++ src/BufferedImageLoader.java
@@ -0,0 +1,16 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class BufferedImageLoader
+{
+    
+    public BufferedImage loadImage(String pathRelativeToThis) throws IOException 
+    {
+        URL url = this.getClass().getResource(pathRelativeToThis);
+        BufferedImage img = ImageIO.read(url);
+        return img;
+    }
+    
+}
